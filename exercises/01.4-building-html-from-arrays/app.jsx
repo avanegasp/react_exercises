75a18f011ea558785efae880b6f78f45f8a82b21
avanegas <arco527@gmail.com>
2025-02-15 10:15:05 -0500

    Add exercise

--- exercises/01.4-building-html-from-arrays/app.jsx
+++ exercises/01.4-building-html-from-arrays/app.jsx
@@ -2,7 +2,25 @@ import React from "react"; // Main React.js library
 import ReactDOM from "react-dom"; // We use ReactDOM to render into the DOM
 
 // Only update the value of this array
-const navlinkItems = [];
+const navlinkItems = [
+	<>
+		<li className="nav-item" key="1">
+			<a className="nav-link" href="https://gitpod.io/workspaces">
+				Link to google.com
+			</a>
+		</li>
+		<li className="nav-item" key="2">
+			<a className="nav-link" href="http://localhost:8000/admin/authenticate/profileacademy/16/change/">
+				Link to facebook.com
+			</a>
+		</li>
+		<li className="nav-item" key="3">
+			<a className="nav-link" href="https://es.react.dev/">
+				Link to amazon.com
+			</a>
+		</li>
+	</>,
+];
 
 const content = <ul className="nav">{navlinkItems}</ul>;
 
