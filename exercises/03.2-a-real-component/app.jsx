1f6c30bfab6fa6977da165982fa88e2ffd2872e6
avanegas <arco527@gmail.com>
2025-02-17 18:12:05 -0500

    Add exercises results

--- exercises/03.2-a-real-component/app.jsx
+++ exercises/03.2-a-real-component/app.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create your function here
+const BootstrapCard = () => {
+	return (
+		<div className="card" style={{ width: "18rem" }}>
+			<img
+				className="card-img-top"
+				src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+				alt="Card image cap"
+			/>
+			<div className="card-body">
+				<h5 className="card-title">Bod Dylan</h5>
+				<p className="card-text">
+					Bob Dylan (legally Robert Dylan;[3] born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.
+				</p>
+				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+};
 
 // Remember to use ReactDOM.render to include the component into the website
-
+ReactDOM.render(<BootstrapCard />, document.querySelector("#myDiv"));
