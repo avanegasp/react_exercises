fd47fb7adcd38d696488fbf717776cc842d723c8
avanegas <arco527@gmail.com>
2025-04-03 12:04:28 -0500

    add results

--- exercises/04.1-conditional-rendering/app.jsx
+++ exercises/04.1-conditional-rendering/app.jsx
@@ -4,6 +4,18 @@ import PropTypes from "prop-types";
 
 const Alert = (props) => {
 	// Your component here
+	if (props.color === "red"){
+		return (
+			<div className="alert alert-danger" role="alert">
+				{props.text}
+			</div>
+		)} else {
+			return (
+				<div className="alert alert-warning" role="alert">
+					{props.text}
+				</div>
+			)
+		}
 };
 Alert.propTypes = {
 	color: PropTypes.string,
